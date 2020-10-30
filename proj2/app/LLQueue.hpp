2a4b7569a92bb81cf92b4791c4d15205f6b6c157
Steven Wu <swimacbook@SwiMacBook.local>
2020-10-30 18:53:27 +0800

    Beta

--- proj2/app/LLQueue.hpp
+++ proj2/app/LLQueue.hpp
@@ -1,7 +1,11 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "modernize-use-nodiscard"
 #ifndef __PROJ2_QUEUE_HPP
 #define __PROJ2_QUEUE_HPP
 
 #include "runtimeexcept.hpp"
+#include <iostream>
+#include <memory>
 
 class QueueEmptyException : public RuntimeException 
 {
@@ -14,13 +18,27 @@ template<typename Object>
 class LLQueue
 {
 private:
-	// fill in private member data here
+    struct Node
+    {
+        Object item;
+        std::shared_ptr<Node> next;
+        explicit Node(Object obj)
+        {
+            item = obj;
+            next = nullptr;
+        }
+    };
+
+    std::shared_ptr<Node> head;
+    std::shared_ptr<Node> rear;
+
+    size_t length;
+
 
 public:
 	LLQueue();
 
-//	These are copy constructors and  assignment operators. 
-	// These should be "deep copies" -- copy the contents, not just a pointer to the front. 
+	// These should be "deep copies" -- copy the contents, not just a pointer to the front.
 	LLQueue(const LLQueue & st);
 	LLQueue & operator=(const LLQueue & st);
 	~LLQueue()
@@ -33,17 +51,111 @@ public:
 
 	// We have two front() for the same reason the Stack in lecture week 2 had two top() functions.
 	// If you do not know why there are two, your FIRST step needs to be to review your notes from that lecture.
-
 	Object & front();
 	const Object & front() const;
 
 	void enqueue(const Object & elem);
 
-// does not return anything.  Just removes. 
+    // does not return anything.  Just removes.
 	void dequeue();
+
+    void print();
 };
 
-// TODO:  Fill in the functions here. 
+// Fill in the functions here.
+template<typename Object>
+LLQueue<Object>::LLQueue()
+    : head{nullptr}, rear{nullptr}, length{0} {
+}
 
+template<typename Object>
+LLQueue<Object>::LLQueue(const LLQueue & st)
+    : head{nullptr}, rear{nullptr}, length{0} {
+    auto current {st.head};
+
+    while (current != nullptr) {
+        enqueue(current->item);
+        current = current->next;
+        length += 1;
+    }
+}
+
+template<typename Object>
+LLQueue<Object>& LLQueue<Object>::operator=(const LLQueue & st) {
+    head = nullptr;
+    rear = nullptr;
+    length = 0;
+
+    auto current {st.head};
+
+    while (current != nullptr) {
+        enqueue(current->item);
+        current = current->next;
+        length += 1;
+    }
+
+    return *this;
+}
+
+
+template<typename Object>
+size_t LLQueue<Object>::size() const noexcept{
+    return length;
+}
+
+template<typename Object>
+bool LLQueue<Object>::isEmpty() const noexcept {
+    return length == 0;
+}
+
+template<typename Object>
+void LLQueue<Object>::print() {
+    auto current {head};
+
+    while (current != nullptr) {
+        std::cout << current->item << " | ";
+        current = current->next;
+    }
+    std::cout << std::endl;
+}
+
+template<typename Object>
+void LLQueue<Object>::enqueue(const Object & elem) {
+    auto temp = std::make_shared<Node>(elem);
+
+    if (head == nullptr) { head = temp; }
+    else { rear->next = temp; }
+
+    rear = temp;
+    length += 1;
+}
+
+template<typename Object>
+void LLQueue<Object>::dequeue() {
+    if (length == 0) {
+        throw QueueEmptyException("The queue is empty");
+    }
+
+    head = head->next;
+    length -= 1;
+}
+
+template<typename Object>
+Object & LLQueue<Object>::front() {
+    if (length == 0) {
+        throw QueueEmptyException("The queue is empty");
+    }
+
+    return head->item;
+}
+
+template<typename Object>
+const Object & LLQueue<Object>::front() const {
+    if (length == 0) {
+        throw QueueEmptyException("The queue is empty");
+    }
+
+    return head->item;
+}
 
 #endif 
--- proj2/app/main.cpp
+++ proj2/app/main.cpp
@@ -1,6 +1,62 @@
+#include "LLQueue.hpp"
+#include <string>
+
 int main()
 {
-    
+    LLQueue<float> queue;
+
+    std::cout << queue.size() << " " << queue.isEmpty() << std::endl;
+    queue.print();
+
+    queue.enqueue(5);
+    queue.enqueue(3);
+    queue.enqueue(1);
+
+    std::cout << queue.size() << " " << queue.isEmpty() << std::endl;
+    queue.print();
+
+    queue.dequeue();
+    queue.dequeue();
+
+    std::cout << queue.size() << " " << queue.isEmpty() << std::endl;
+    queue.print();
+
+    std::cout << std::endl;
+
+    LLQueue<std::string> squeue;
+
+    std::cout << squeue.size() << " " << squeue.isEmpty() << std::endl;
+    squeue.print();
+
+    squeue.enqueue("nihao");
+    squeue.enqueue("hen");
+    squeue.enqueue("meili");
+
+    auto ssqueue {squeue};
+
+    std::cout << ssqueue.front() << " " << ssqueue.front() << std::endl;
+    ssqueue.print();
+
+    std::cout << squeue.front() << " " << squeue.front() << std::endl;
+    squeue.print();
+
+    squeue.dequeue();
+    squeue.dequeue();
+
+    std::cout << squeue.front() << " " << squeue.front() << std::endl;
+    squeue.print();
+
+    auto ueue {squeue};
+    std::cout << ueue.front() << " " << ueue.front() << std::endl;
+    ueue.print();
+
+    std::cout << ssqueue.front() << " " << ssqueue.front() << std::endl;
+    ssqueue.print();
+
+    ssqueue = ueue;
+
+    std::cout << ssqueue.front() << " " << ssqueue.front() << std::endl;
+    ssqueue.print();
+
     return 0;
 }
-
